a62d77c6ea5865c0359fd8a78b8b7faae25e5b1f
Allen Krupic <110694062+heyitssuga@users.noreply.github.com>
2026-01-30 22:04:53 -0700

    Trying to make work :(

--- gameJam2026/Scripts/Enemy_AI.gd
+++ gameJam2026/Scripts/Enemy_AI.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+@onready var ray_cast: RayCast2D = $RayCast2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,4 +14,21 @@ func _physics_process(delta):
 	var query = PhysicsRayQueryParameters2D.create(Vector2(0, 0), Vector2(50, 100))
 	var result = space_state.intersect_ray(query)
 	
-	print(result.collider)
+	if result:
+		print(result.collider)
+
+
+func _draw():
+	var start_point = Vector2.ZERO
+	var end_point = ray_cast.target_position
+	var color = Color(1, 0, 0) # Red if not colliding
+	if ray_cast.is_colliding():
+		end_point = ray_cast.get_collision_point()
+		color = Color(0, 1, 0) # Green if colliding
+
+	# Draw the line
+	draw_line(start_point, end_point, color, 2.0)
+	
+	# Draw a point at the end
+	if ray_cast.is_colliding():
+		draw_circle(end_point, 4.0, Color(0, 1, 0))
