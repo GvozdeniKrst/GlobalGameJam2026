67d242025758ca511442c7b7e2ac1d16bccb7305
Allen Krupic <110694062+heyitssuga@users.noreply.github.com>
2026-01-31 04:12:00 -0700

    Enemy ai basic done

--- gameJam2026/Scripts/Enemy_AI.gd
+++ gameJam2026/Scripts/Enemy_AI.gd
@@ -1,5 +1,5 @@
 extends CharacterBody2D
-
+var direction = 360
 @onready var ray_cast: RayCast2D = $RayCast2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,10 +9,12 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	$RayCast2D.enabled = true
-	
+	velocity.x = direction
 	if ray_cast.is_colliding():
 		var collider = ray_cast.get_collider()
-		if collider.name != "JohnEnemy":
-			print("hi")
+		if collider.name == "Player":
+			print("ATTACK")
 		else:
-			print("KILL HIM")
+			direction *= -1
+			ray_cast.target_position *= -1
+	move_and_slide()
