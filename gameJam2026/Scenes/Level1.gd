18a3c35951818dc04657c92a2a2e79c5cb5c189d
Adam Germain <adam.germain@outlook.com>
2026-01-31 22:38:03 -0700

    Created red mask object

--- gameJam2026/Scenes/Level1.gd
+++ gameJam2026/Scenes/Level1.gd
@@ -4,3 +4,7 @@ extends Node2D
 func _on_door_to_level_2_body_entered(body): 
 	get_tree().change_scene_to_file("res://Scenes/Level2.tscn")
 	
+
+
+func _on_red_mask_body_entered(body):
+	print("Sample")
