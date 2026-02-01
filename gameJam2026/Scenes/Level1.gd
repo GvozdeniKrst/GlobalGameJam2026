c919e7b1a0b66d39a5f505af74f67eb5e955ac02
Stefan Cvijovic <cvijovics@mymacewan.ca>
2026-02-01 05:27:37 -0700

    added some stuff to player script
    added logic to update player portrait when grabbing mask

--- gameJam2026/Scenes/Level1.gd
+++ gameJam2026/Scenes/Level1.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 
 func _on_door_to_level_2_body_entered(body): 
-	get_tree().change_scene_to_file("res://Scenes/Level2.tscn")
+	get_tree().change_scene_to_file("res://Scenes/level_transition.tscn")
 	
 
 
@@ -10,5 +10,6 @@ func _on_red_mask_body_entered(body):
 	var orphaned_node = get_node("RedMask")
 	remove_child(orphaned_node)
 	print("carsick")
+	
 	# You can re-add it later
 	# add_child(orphaned_node)
--- gameJam2026/Scripts/Player.gd
+++ gameJam2026/Scripts/Player.gd
@@ -8,6 +8,7 @@ const ATTACKFRICTION := 1200
 
 const MAXHP := 2
 var current_hp := MAXHP
+var hasMask := false
 
 @onready var state_machine = $StateMachine
 @onready var sprite = $Sprite
@@ -17,7 +18,7 @@ var current_hp := MAXHP
 
 func _ready() -> void:
 	portrait_sprite.frame = 0
-	
+
 func handle_platform_fallthrough():
 	if Input.is_action_pressed("below"):
 		set_collision_mask_value(5, false)
@@ -26,10 +27,12 @@ func handle_platform_fallthrough():
 		set_collision_mask_value(5, true)
 		
 func _physics_process(delta):
+	var count := 0
 	velocity.y += GRAVITY
 	state_machine.physics_update(delta)
 	handle_platform_fallthrough()
 	move_and_slide()
+	
 
 func _unhandled_input(event):
 	state_machine.handle_input(event)
@@ -38,15 +41,23 @@ func take_damage(amount: int):
 	if current_hp <= 0:
 		return
 	
-	
 	current_hp -= amount
 	
-	portrait_sprite.frame = 2
-	# if mask is equipped, swap to 5
+	if !hasMask:
+		portrait_sprite.frame = 2
+	else:
+		portrait_sprite.frame = 5
+	
 	timer_node.start()
 	if current_hp <= 0:
 		state_machine.change_state($StateMachine/Dead)
+		
 
+func wear_mask():
+	hasMask = true
 
 func _on_timer_timeout() -> void:
-	portrait_sprite.frame = 1 # or 4 if wearing mask
+	if(!hasMask):
+		portrait_sprite.frame = 1
+	else:
+		portrait_sprite.frame = 4
